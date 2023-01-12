588a9abcbb56f0a1bb5c4be817606ee8b79992f1
Mylena Araújo <47645536+myllakassia@users.noreply.github.com>
2023-01-12 13:25:18 -0300

    adicionando o ts e incrementando aa forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Executei!");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "sucesso total! vocês são demais" });
+});
+
+//subindo no servido atraves da porta 3000
+app.listen(3000, () => {
+  console.log("Aplicação executando na porta 3000");
+});
